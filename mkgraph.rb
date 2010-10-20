ca6cf09f304f8e191414c2a4f48323be41779dd1
Duane Johnson <duane.johnson@gmail.com>
2010-10-19 20:56:36 -0500

    Created ruby script to convert CSV to dot file

---
+++ mkgraph.rb
@@ -0,0 +1,62 @@
+require 'rubygems'
+require 'csv'
+
+if ARGV.size <= 1
+  puts "Usage: mkgraph.rb [election.csv] [candidate-1] [candidate-2] ... [candidate-n]"
+else
+  $options = ARGV[1..-1]
+end
+
+template = <<-EOF
+  digraph g {
+    graph [
+      rankdir = "LR"
+    ];
+
+    node [
+      shape = "Mrecord"
+    ];
+    
+%s
+    
+    %s
+  }
+EOF
+
+def prefs_array(prefs)
+  arr = []
+  prefs.each_with_index do |pref, i|
+    (arr[pref] ||= []) << $options[i]
+  end
+  arr
+end
+
+nodes = []
+edges = []
+CSV.foreach(ARGV[0] || "election.csv", :headers => :first_row, :return_headers => false) do |row|
+  if row["DelegateID"]
+    edges << <<-ROW
+      "#{row["ID"]}" -> "#{row["DelegateID"]}"
+    ROW
+  end
+  if row["Preferences"]
+    tr = %{<tr><td align="left">%s</td></tr>}
+    prefs = row["Preferences"].split("|").map{ |x| x.to_i }
+    pref_rows = prefs_array(prefs).map{ |p| tr % p.join(", ") }.join("")
+    nodes << <<-ROW
+      "#{row["ID"]}" [
+        label = <<table border="0" cellborder="0" cellpadding="3" bgcolor="white"><tr><td bgcolor="black" align="center" colspan="2"><font color="white">#{row["Name"]}</font></td></tr>#{pref_rows}</table>>
+      ];
+    ROW
+  else
+    nodes << <<-ROW
+      "#{row["ID"]}" [
+        label = <<table border="0" cellborder="0" cellpadding="3" bgcolor="black"><tr><td bgcolor="black" align="center" colspan="2"><font color="white">#{row["Name"]}</font></td></tr></table>>
+        style = "filled"
+        fillcolor = "black"
+      ];
+    ROW
+  end
+end
+
+puts(template % [nodes.join("\n"), edges.join("\n")])
